f370e09d87a8523491219bcd1e65eb592428899e
Femi Olamijulo <olufemi_olamijulo@gsd.harvard.edu>
2025-10-02 23:01:42 -0400

    ci: build & deploy Astro to cPanel via FTPS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,4 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-
-// https://astro.build/config
 export default defineConfig({
-  site: "https://nexuseng.org",
-  // base: "/SUBFOLDER", // Set this for deploying to a subfolder
-  integrations: [mdx(), sitemap()],
+  site: 'https://nexuseng.org', // no "base" for root
 });
